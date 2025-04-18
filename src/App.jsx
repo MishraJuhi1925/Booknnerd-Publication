0a21539cb3673171d246defa9b5c653e2f58c28c
MishraJuhi9125 <juhimishra9125@gmail.com>
2025-04-18 10:15:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Home from './component/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import HeroSection from './component/HeroSection';
+import ServiceCards from './component/ServiceCards';
+import Recognitions from './component/Recognitions';
+import About from './component/About';
+import Contact from './component/Contact';
+import ServiceDetail from './component/ServiceDetail';
+import ScrollToTop from './component/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <ServiceCards />
+              <Recognitions />
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services/:slug" element={<ServiceDetail />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
